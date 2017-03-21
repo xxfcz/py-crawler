94b557bff4b3781989297180ef73b4854bc61f34
xxfcz <xxf_cz@163.com>
2017-03-21 16:43:26 +0800

    save data to file

--- webscraping.py
+++ webscraping.py
@@ -2,6 +2,7 @@ import download
 import re
 import lxml.html
 import mongo_cache
+import csv
 
 FIELDS = ('area', 'population', 'iso', 'country', 'capital', 'continent', 'tld', 'currency_code', 'currency_name',
           'phone', 'postal_code_format', 'postal_code_regex', 'languages', 'neighbours');
@@ -24,6 +25,22 @@ def cb_print2(url, html):
                for field in FIELDS]
         print url, row
 
+# Save extracted data to .cvs file
+class ScrapeCallback:
+    def __init__(self):
+        self.writer = csv.writer(open('countries.csv', 'w'))
+        self.fields = FIELDS
+        self.writer.writerow(self.fields)
+
+    def __call__(self, url, html):
+        if re.search('/view/', url):
+            tree = lxml.html.fromstring(html)
+            row = [tree.cssselect('#places_%s__row > td.w2p_fw' % field)[0].text_content()
+                   for field in FIELDS]
+            print row
+            self.writer.writerow(row)
+
 
 cache = mongo_cache.MongoCache()
-download.link_crawler('http://example.webscraping.com/', '/(index|view)', delay=-1, callback=cb_print2, cache=cache)
+cb = ScrapeCallback()
+download.link_crawler('http://example.webscraping.com/', '/(index|view)', delay=-1, callback=cb, cache=cache)
